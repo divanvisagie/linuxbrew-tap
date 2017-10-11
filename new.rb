a51579283a7ec7010a7884553b1ffd70ff782d78
Divan Visagie <me@dvisagie.com>
2017-10-11 11:17:21 +0200

    Update new.rb

--- new.rb
+++ new.rb
@@ -5,8 +5,8 @@
 class New < Formula
   desc "new is a command to create projects off of github seed repos"
   homepage "https://github.com/divanvisagie/new"
-  url "https://github.com/divanvisagie/new/releases/download/v0.0.6/new_0.0.6_darwin_amd64.tar.gz"
-  sha256 "99701990db3d241aea9417a01173fce44e465064fee3665690c3fbbbeb4481e0"
+  url "https://github.com/divanvisagie/new/releases/download/v0.0.6/new_0.0.6_linux_amd64.tar.gz"
+  sha256 "803eec9edbbfcd73e6495d4191c0939b1953f76cf3d0f89a30374faedbbcf953"
 
   def install
      bin.install "new"
